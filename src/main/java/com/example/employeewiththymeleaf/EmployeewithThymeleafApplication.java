ae10274c3710d5e9afd70b55cfd14cceb9cdc77b
Yves <yxn16a@acu.edu>
2022-05-16 01:26:52 +0200

    Initial commit

---
+++ src/main/java/com/example/employeewiththymeleaf/EmployeewithThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeewiththymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeewithThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeewithThymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/employeewiththymeleaf/EmployeewithThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeewiththymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeewithThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
